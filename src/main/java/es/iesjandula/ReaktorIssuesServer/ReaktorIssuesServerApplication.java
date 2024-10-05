d4ebee7b7f069295da6b3f6ba6e16577c1f58d65
Gedus04 <eduardomorenoalgaba2004@gmail.com>
2024-10-05 11:50:36 +0200

    creacion del repositorio

---
+++ src/main/java/es/iesjandula/ReaktorIssuesServer/ReaktorIssuesServerApplication.java
@@ -0,0 +1,13 @@
+package es.iesjandula.ReaktorIssuesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaktorIssuesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaktorIssuesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/iesjandula/ReaktorIssuesServer/ReaktorIssuesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.ReaktorIssuesServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaktorIssuesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
